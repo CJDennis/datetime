0960e2f8d7e3f53d5dc61c2b7ca8b8d78fec3cb9
Daniel Klein <danielklein@airpost.net>
2019-09-21 21:56:07 +1000

    Pass the test

--- src/CjDennis/DateTime/DateTimeNanosecond.php
+++ src/CjDennis/DateTime/DateTimeNanosecond.php
@@ -13,12 +13,26 @@ class DateTimeNanosecond extends DateTime {
 
   /** @noinspection PhpMissingParentConstructorInspection */
   public function __construct($time = 'now', DateTimeZone $timezone = null) {
+    static $fractional_seconds_pattern = '/(.*)(\d\.(?>\d{1,7})\d*?)(\d{0,2}(?!\d))(.*)/';
+    $match = null;
+
+    $matched = preg_match($fractional_seconds_pattern, $time, $match);
+    if ($matched) {
+      $time = $match[1] . $match[2] . $match[4];
+      $fractional_seconds = (float)($match[2] . $match[3]);
+    }
+
     $date = new parent($time, $timezone);
     $this->hidden_value($date);
     foreach ((array)$date as $name => $value) {
       $this->$name = $value;
     }
-    $this->date .= '000';
+
+    if (!$matched) {
+      preg_match($fractional_seconds_pattern, $this->date, $match);
+      $fractional_seconds = (float)($match[2] . $match[3]);
+    }
+    $this->date = sprintf('%s%11.9f%s', $match[1], $fractional_seconds, $match[4]);
   }
 
   public function hidden_value() {
