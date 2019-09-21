46709e98445fc87012246b8561f4aa8c9639ef79
Daniel Klein <danielklein@airpost.net>
2019-09-21 19:44:30 +1000

    Add a test to check that microseconds are extended to nanoseconds

--- tests/unit/CjDennis/DateTime/DateTimeNanosecondTest.php
+++ tests/unit/CjDennis/DateTime/DateTimeNanosecondTest.php
@@ -3,10 +3,11 @@ namespace CjDennis\DateTime;
 
 use Codeception\Test\Unit;
 use DateTime;
+use UnitTester;
 
 class DateTimeNanosecondTest extends Unit {
   /**
-   * @var \UnitTester
+   * @var UnitTester
    */
   protected $tester;
 
@@ -20,5 +21,14 @@ class DateTimeNanosecondTest extends Unit {
   public function testShouldReturnAnInstanceOfDateTime() {
     $date_time_nanosecond = new DateTimeNanosecond();
     $this->assertInstanceOf(DateTime::class, $date_time_nanosecond);
+  }
+
+  public function testShouldExtendMicrosecondsToNanoseconds() {
+    $this->assertSame('UTC', date_default_timezone_get());
+    $date_time_nanosecond = new DateTimeNanosecond('2021-12-23 23:34:45.123456');
+    $this->assertSame(
+      'O:36:"CjDennis\DateTime\DateTimeNanosecond":3:{s:4:"date";s:29:"2021-12-23 23:34:45.123456000";s:13:"timezone_type";i:3;s:8:"timezone";s:3:"UTC";}',
+      serialize($date_time_nanosecond)
+    );
   }
 }
