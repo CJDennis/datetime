cfb428c0082f54e18309f75e5d525e72579f6984
Daniel Klein <danielklein@airpost.net>
2019-09-22 12:28:45 +1000

    Add a test to check that nine decimal places can be saved in DateTimeNanosecond::$f

--- tests/unit/CjDennis/DateTime/DateTimeNanosecondIntervalTest.php
+++ tests/unit/CjDennis/DateTime/DateTimeNanosecondIntervalTest.php
@@ -21,4 +21,10 @@ class DateTimeNanosecondIntervalTest extends Unit {
   public function testShouldCheckThatADateTimeNanosecondIntervalIsAnInstanceOfDateInterval() {
     $this->assertInstanceOf(DateInterval::class, new DateTimeNanosecondInterval('P1D'));
   }
+
+  public function testShouldCheckThatFractionalSecondsCanContainUpToNineDecimalPlaces() {
+    $date_time_nanosecond_interval = new DateTimeNanosecondInterval('P1D');
+    $date_time_nanosecond_interval->f = 0.123456789;
+    $this->assertSame(0.123456789, $date_time_nanosecond_interval->f);
+  }
 }
