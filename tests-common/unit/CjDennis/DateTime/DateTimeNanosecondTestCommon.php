3081b5f8b77f6ee32e25f2c4956a21e316098243
Daniel Klein <danielklein@airpost.net>
2019-10-02 10:10:04 +1000

    Add tests to kill mutants

--- tests-common/unit/CjDennis/DateTime/DateTimeNanosecondTestCommon.php
+++ tests-common/unit/CjDennis/DateTime/DateTimeNanosecondTestCommon.php
@@ -51,6 +51,12 @@ trait DateTimeNanosecondTestCommon {
     );
   }
 
+  public function testShouldBeAbleToAccessTheMethodsOfAnUnserialisedDateTimeNanosecondObject() {
+    /** @var DateTimeNanosecond $date_time_nanosecond */
+    $date_time_nanosecond = unserialize('O:36:"CjDennis\DateTime\DateTimeNanosecond":3:{s:4:"date";s:29:"2021-12-23 23:34:45.123456789";s:13:"timezone_type";i:3;s:8:"timezone";s:3:"UTC";}');
+    $this->assertSame('2021-12-23 23:34:45.123456789', $date_time_nanosecond->format('Y-m-d H:i:s.X'));
+  }
+
   public function testShouldFormatADateTimeNanosecondWithMilliseconds() {
     $date_time_nanosecond = new DateTimeNanosecond('12:34:56.123456789');
     $this->assertSame('56.123456', $date_time_nanosecond->format('s.u'));
@@ -115,6 +121,19 @@ trait DateTimeNanosecondTestCommon {
     $len = strlen($fully_qualified_name);
     $s = '1';
     $f = '1.0E-9';
+    $invert = '0';
+    $this->assertSame('O:' . $len . ':"' . $fully_qualified_name . '":16:{s:1:"y";i:0;s:1:"m";i:0;s:1:"d";i:0;s:1:"h";i:0;s:1:"i";i:0;s:1:"s";i:' . $s . ';s:1:"f";d:' . $f . ';s:7:"weekday";i:0;s:16:"weekday_behavior";i:0;s:17:"first_last_day_of";i:0;s:6:"invert";i:' . $invert . ';s:4:"days";i:0;s:12:"special_type";i:0;s:14:"special_amount";i:0;s:21:"have_weekday_relative";i:0;s:21:"have_special_relative";i:0;}', serialize($date_time_nanosecond_interval));
+  }
+
+  public function testShouldGetAOneSecondDifference() {
+    $date_time_nanosecond_1 = new DateTimeNanosecond('2001-01-01 01:01:01.000000001');
+    $date_time_nanosecond_2 = new DateTimeNanosecond('2001-01-01 01:01:02.000000001');
+    $date_time_nanosecond_interval = $date_time_nanosecond_1->diff($date_time_nanosecond_2);
+
+    $fully_qualified_name = DateTimeNanosecondInterval::class;
+    $len = strlen($fully_qualified_name);
+    $s = '1';
+    $f = '0';
     $invert = '0';
     $this->assertSame('O:' . $len . ':"' . $fully_qualified_name . '":16:{s:1:"y";i:0;s:1:"m";i:0;s:1:"d";i:0;s:1:"h";i:0;s:1:"i";i:0;s:1:"s";i:' . $s . ';s:1:"f";d:' . $f . ';s:7:"weekday";i:0;s:16:"weekday_behavior";i:0;s:17:"first_last_day_of";i:0;s:6:"invert";i:' . $invert . ';s:4:"days";i:0;s:12:"special_type";i:0;s:14:"special_amount";i:0;s:21:"have_weekday_relative";i:0;s:21:"have_special_relative";i:0;}', serialize($date_time_nanosecond_interval));
   }
