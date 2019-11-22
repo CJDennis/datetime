16ee1a0d34cac9965d9b1fd4be0ebb82583c5f9d
Daniel Klein <danielklein@airpost.net>
2019-11-22 16:38:44 +1100

    Move files from CjDennis to CJDennis

---
+++ tests-codeception/unit/CJDennis/DateTime/DateTimeNanosecondTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace CJDennis\DateTime;
+
+use Codeception\Test\Unit;
+use UnitTester;
+
+class DateTimeNanosecondTest extends Unit {
+  /**
+   * @var UnitTester
+   */
+  protected $tester;
+
+  use DateTimeNanosecondTestCommon;
+}
